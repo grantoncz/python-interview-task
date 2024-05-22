3cc89dd5d2f013a096a9c13a963968cfee094c9f
jackeric <jackery.outlook@gmail.com>
2024-05-22 16:58:27 +1000

    minor bug fixes

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
-
+# load open ai key from env
 class Config:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
--- text_classifier.py
+++ text_classifier.py
@@ -97,4 +97,4 @@ def classify():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
